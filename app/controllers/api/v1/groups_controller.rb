8ccb3cbc077aac5ac4fd8cf17dc0ac959f2bac91
rafli <rafliyusuff277@gmail.com>
2023-05-10 15:00:38 +0700

    menambahkan group controller

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::GroupsController < ApplicationController
+    def index
+        render jsonapi: Group.all
+    end
+
+    def show
+        @group = Group.find(params[:id])
+        render jsonapi: @group
+    end
+
+    def create
+        @group = Group.new(param_group)
+        if @group.save
+            render json: {data: 'berhasil'}
+        else
+            render json: {data: 'gagal'}
+        end
+    end
+
+    def update
+        # binding.pry
+        @group = Group.find(params[:id])
+        if @group.update(param_group)
+          render json: {data: 'Berhasil update'}
+        else
+          render json: {data: 'Gagal update'}
+        end
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        if @group.destroy
+            render json: {data: 'Berhasil Hapus'}
+        else
+            render json: {data: 'Gagal Hapus'}
+        end
+    end
+    
+    private 
+    def param_group
+      params.require(:group).permit(:name)
+    end 
+
+end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
     has_many :user
     has_many :gate_groups
+    validates :name ,uniqueness: true
 end
