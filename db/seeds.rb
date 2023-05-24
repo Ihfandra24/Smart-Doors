bc18ac87b7193a3a15923156d6267f6f18b9b5a9
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-05-24 14:39:34 +0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ Group.find_or_create_by(name: "Marketing")
 Group.find_or_create_by(name: "IT")
 Group.find_or_create_by(name: "Gudang")
 
-User.find_or_create_by(name: "Ihfazriel", group_id: Group.first.id, email: "soko@gmail.com", password_digest: "12345", phone: "093820398")
-User.find_or_create_by(name: "Yoyo", group_id: Group.last.id, email: "aoks@gmail.com",password_digest: "2345", phone: "091209102")
+User.find_or_create_by(name: "Ihfazriel", group_id: Group.first.id, email: "soko@gmail.com",  phone: "093820398").update(password: "12345")
+User.find_or_create_by(name: "Yoyo", group_id: Group.last.id, email: "aoks@gmail.com", phone: "091209102").update(password: "67890")
 
 GateGroup.find_or_create_by(gate_id: 1, group_id: 2)
 
