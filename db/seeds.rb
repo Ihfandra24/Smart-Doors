f507d380787df1897e90fa56a0882073cabfde93
Mirzalazuardi Hermawan <mirzalazuardi@users.noreply.github.com>
2023-05-26 17:00:11 +0700

    add rfid access from to

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ Group.find_or_create_by(name: "Gudang")
 
 User.create!(name: "Ihfazriel", group_id: Group.first.id, email: "soko@gmail.com",  phone: "093820398", password: "12345", rfid: '44444')
 User.create!(name: "Yoyo", group_id: Group.last.id, email: "aoks@gmail.com", phone: "091209102", password: "67890", rfid: '3333')
+User.create!(name: "Mirza", group_id: Group.find_by(name: "IT").id, email: "mirzalazuardi@gmail.com", phone: "081573088881", password: "12345678", rfid: '1111')
 
 Gate.find_or_create_by(name: "IT Doors", rfid: '55555')
 Gate.find_or_create_by(name: "Marketing Doors", rfid: '66666')
